a1993a2c3d2ae1359ad770eadbd0971d63e3cb0c
Kid-Chang <pch3310@gmail.com>
2024-03-05 15:05:19 +0900

    feat: swagger 적용

--- src/main/java/com/kidchang/lingopress/translate/TranslateController.java
+++ src/main/java/com/kidchang/lingopress/translate/TranslateController.java
@@ -3,6 +3,7 @@ package com.kidchang.lingopress.translate;
 import com.kidchang.lingopress._base.response.DataResponseDto;
 import com.kidchang.lingopress.translate.dto.request.TranslateTextRequest;
 import com.kidchang.lingopress.translate.dto.response.TranslateTextResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,12 +19,14 @@ public class TranslateController {
 
     private final TranslateService translateService;
 
+
+    // DeepL을 이용한 번역은 2차 스프린트에서는 제거 후, 뉴스를 저장할 때 번역본도 다른 테이블에 저장하는 방식으로 변경 예정
+    @Operation(summary = "DeepL로 번역")
     @PostMapping("/machine")
     public DataResponseDto<TranslateTextResponse> translateTextInMachine(
         @RequestBody TranslateTextRequest originalText) {
 
         return DataResponseDto.of(translateService.translate(originalText));
-
     }
 
 }
