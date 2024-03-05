bec29a7de0025ff59226c3b4388615dc33b3c6c2
Kid-Chang <pch3310@gmail.com>
2024-03-05 15:55:03 +0900

    feat: user controller, repository, service 뼈대 제작

---
+++ src/main/java/com/kidchang/lingopress/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.kidchang.lingopress.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
---
+++ src/main/java/com/kidchang/lingopress/user/UserService.java
@@ -0,0 +1,12 @@
+package com.kidchang.lingopress.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public String getUser() {
+        return "user";
+    }
+
+}
