779798f86ecf460cac49e9ed7d5972b1393aff14
Kid-Chang <pch3310@gmail.com>
2024-03-10 21:08:22 +0900

    fix: JPA스럽게? 사용하기위해 Long pressId ->Press press 사용

--- src/main/java/com/kidchang/lingopress/press/entity/PressContentLine.java
+++ src/main/java/com/kidchang/lingopress/press/entity/PressContentLine.java
@@ -2,9 +2,12 @@ package com.kidchang.lingopress.press.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity
@@ -14,7 +17,9 @@ public class PressContentLine {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long pressId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "press_id")
+    private Press press;
     private Integer lineNumber;
     @Column(columnDefinition = "TEXT")
     private String lineText;
