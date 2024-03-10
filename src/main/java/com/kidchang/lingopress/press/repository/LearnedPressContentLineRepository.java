cde6f60b556c0ab79cba20502bc56cab322a5244
Kid-Chang <pch3310@gmail.com>
2024-03-10 21:10:40 +0900

    feat: JPQL와 outer join을 이용한 findByUserAndPressAndPressContent 메서드 구현.
    ** outer join을 통해 얻은 값을 새로운 response record에 넣어보기.

--- src/main/java/com/kidchang/lingopress/press/repository/LearnedPressContentLineRepository.java
+++ src/main/java/com/kidchang/lingopress/press/repository/LearnedPressContentLineRepository.java
@@ -1,12 +1,14 @@
 package com.kidchang.lingopress.press.repository;
 
+import com.kidchang.lingopress.press.dto.response.PressContentLineResponse;
 import com.kidchang.lingopress.press.entity.LearnedPress;
 import com.kidchang.lingopress.press.entity.LearnedPressContentLine;
 import com.kidchang.lingopress.press.entity.PressContentLine;
-import com.kidchang.lingopress.user.User;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,6 +18,25 @@ public interface LearnedPressContentLineRepository extends
     Optional<LearnedPressContentLine> findByLearnedPressAndPressContent(LearnedPress learnedPress,
         PressContentLine pressContent);
 
-    List<LearnedPressContentLine> findByUserAndLearnedPress(User user,
-        LearnedPress learnedPress);
+// 내가 짠 코드
+//    @Query(
+//        "SELECT new com.kidchang.lingopress.press.dto.response.PressContentLineResponse(pcl.id, pcl.lineNumber, lpcl.userTranslatedLine, pcl.translatedLineText, pcl.lineText, lpcl.isCorrect) "
+//            + "FROM PressContentLine AS pcl "
+//            + "left outer JOIN LearnedPressContentLine AS lpcl ON pcl.press.id = :pressId  "
+//            + "WHERE lpcl.user.id = :userId AND lpcl.pressContentLineNumber = pcl.lineNumber")
+// gpt 수정 원본
+//    @Query(
+//        "SELECT new com.kidchang.lingopress.press.dto.response.PressContentLineResponse(pcl.id, pcl.lineNumber, lpcl.userTranslatedLine, pcl.translatedLineText, pcl.lineText, CASE WHEN lpcl.id IS NULL THEN false ELSE lpcl.isCorrect END) "
+//            + "FROM PressContentLine AS pcl "
+//            + "LEFT OUTER JOIN LearnedPressContentLine AS lpcl ON lpcl.pressContentLineNumber = pcl.lineNumber AND lpcl.user.id = :userId "
+//            + "WHERE pcl.press.id = :pressId")
+
+    // gpt 수정 수정본
+    @Query(
+        "SELECT new com.kidchang.lingopress.press.dto.response.PressContentLineResponse(pcl.id, pcl.lineNumber, lpcl.userTranslatedLine, pcl.translatedLineText, pcl.lineText, lpcl.isCorrect) "
+            + "FROM PressContentLine AS pcl "
+            + "LEFT OUTER JOIN LearnedPressContentLine AS lpcl ON lpcl.pressContentLineNumber = pcl.lineNumber AND lpcl.user.id = :userId "
+            + "WHERE pcl.press.id = :pressId")
+    List<PressContentLineResponse> findByUserAndPressAndPressContent(@Param("userId") Long user,
+        @Param("pressId") Long pressId);
 }
