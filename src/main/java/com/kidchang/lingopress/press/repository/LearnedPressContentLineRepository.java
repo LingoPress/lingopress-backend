2aeb8b0cd773c6871970a2d3be8d7bbc79676bee
Kid-Chang <pch3310@gmail.com>
2024-03-11 14:29:27 +0900

    fix: * pressId에 맞춰 리턴되도록 수정.

--- src/main/java/com/kidchang/lingopress/press/repository/LearnedPressContentLineRepository.java
+++ src/main/java/com/kidchang/lingopress/press/repository/LearnedPressContentLineRepository.java
@@ -36,7 +36,7 @@ public interface LearnedPressContentLineRepository extends
     @Query(
         "SELECT new com.kidchang.lingopress.press.dto.response.PressContentLineResponse(pcl.id, pcl.lineNumber, lpcl.userTranslatedLine, pcl.translatedLineText, pcl.lineText, lpcl.isCorrect) "
             + "FROM PressContentLine AS pcl "
-            + "LEFT OUTER JOIN LearnedPressContentLine AS lpcl ON lpcl.lineNumber = pcl.lineNumber AND lpcl.user.id = :userId "
+            + "LEFT OUTER JOIN LearnedPressContentLine AS lpcl ON lpcl.lineNumber = pcl.lineNumber AND lpcl.user.id = :userId AND lpcl.press.id = :pressId "
             + "WHERE pcl.press.id = :pressId")
     List<PressContentLineResponse> findByUserAndPressAndPressContent(@Param("userId") Long user,
         @Param("pressId") Long pressId);
