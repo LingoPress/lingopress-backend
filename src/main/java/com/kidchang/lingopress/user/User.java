fb39083ad602280e46e0cd6a0c58ab6e3c550a8b
Kid-Chang <pch3310@gmail.com>
2024-03-17 12:20:26 +0900

    chore: User의 GeneratedValue 전략을 명시

--- src/main/java/com/kidchang/lingopress/user/User.java
+++ src/main/java/com/kidchang/lingopress/user/User.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -20,7 +21,7 @@ import lombok.NoArgsConstructor;
 public class User extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     // provider + id
