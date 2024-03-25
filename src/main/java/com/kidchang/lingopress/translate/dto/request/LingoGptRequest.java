3e1bef1b802e3217a40329f9ca4d3a0fc140c4e4
Kid-Chang <pch3310@gmail.com>
2024-03-25 12:38:18 +0900

    fix: LingoGptRequest의 인자 text를 original_text로 변경

--- src/main/java/com/kidchang/lingopress/translate/dto/request/LingoGptRequest.java
+++ src/main/java/com/kidchang/lingopress/translate/dto/request/LingoGptRequest.java
@@ -1,11 +1,13 @@
 package com.kidchang.lingopress.translate.dto.request;
 
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class LingoGptRequest {
 
-    private String text;
+    private String original_text;
     private String word;
 }
