b053edc6a33cd8eb202abecdeb19bca379f9f2b6
Kid-Chang <pch3310@gmail.com>
2024-03-31 13:41:49 +0900

    feat: 단어 뜻 수정, 삭제 기능 구현

--- src/main/java/com/kidchang/lingopress/word/WordController.java
+++ src/main/java/com/kidchang/lingopress/word/WordController.java
@@ -4,15 +4,11 @@ import com.kidchang.lingopress._base.response.DataResponseDto;
 import com.kidchang.lingopress.word.dto.request.WordToLearnRequest;
 import com.kidchang.lingopress.word.dto.response.WordToLearnResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -24,14 +20,14 @@ public class WordController {
 
     @PostMapping("/need-to-learn")
     public DataResponseDto<WordToLearnResponse> enrollWordToLearn(
-        @RequestBody WordToLearnRequest request
+            @RequestBody WordToLearnRequest request
     ) {
         return DataResponseDto.of(wordService.enrollWordToLearn(request));
     }
 
     @GetMapping("/need-to-learn/{pressId}")
     public DataResponseDto<List<WordToLearnResponse>> getWordToLearn(
-        @PathVariable Long pressId
+            @PathVariable Long pressId
     ) {
         return DataResponseDto.of(wordService.getWordToLearn(pressId));
     }
@@ -40,6 +36,23 @@ public class WordController {
     @GetMapping("/need-to-learn")
     public DataResponseDto<List<WordToLearnResponse>> getMyWords() {
         return DataResponseDto.of(wordService.getWordToLearn());
+    }
+
+    @Operation(summary = "단어 수정")
+    @PutMapping("/{wordId}")
+    public DataResponseDto<String> updateWord(
+            @PathVariable Long wordId,
+            @RequestBody WordToLearnRequest translatedWord
+    ) {
+        return DataResponseDto.of(wordService.updateWord(wordId, translatedWord));
+    }
+
+    @Operation(summary = "단어 삭제")
+    @DeleteMapping("/{wordId}")
+    public DataResponseDto<String> deleteWord(
+            @PathVariable Long wordId
+    ) {
+        return DataResponseDto.of(wordService.deleteWord(wordId));
     }
 
 
--- src/main/java/com/kidchang/lingopress/word/WordService.java
+++ src/main/java/com/kidchang/lingopress/word/WordService.java
@@ -8,10 +8,13 @@ import com.kidchang.lingopress.translate.dto.response.TranslateTextResponse;
 import com.kidchang.lingopress.user.UserRepository;
 import com.kidchang.lingopress.word.dto.request.WordToLearnRequest;
 import com.kidchang.lingopress.word.dto.response.WordToLearnResponse;
-import java.util.List;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -37,39 +40,39 @@ public class WordService {
 
         // 3. 뜻 해석하기
         TranslateTextResponse translateTextResponse = translateService.translateWithUsageTracker(
-            LingoGptRequest.builder()
-                .original_text(request.originalText())
-                .word(request.word())
-                .build());
+                LingoGptRequest.builder()
+                        .original_text(request.originalText())
+                        .word(request.word())
+                        .build());
 
         WordToLearn wordToLearn = WordToLearn.builder()
-            // .user(user)
-            .userId(userId)
-            .word(request.word())
-            .originalLineText(request.originalText())
-            // .press(press)
-            .pressId(request.pressId())
-            .lineNumber(request.lineNumber())
-            .translatedWord(translateTextResponse.getTranslatedText())
-            .build();
+                // .user(user)
+                .userId(userId)
+                .word(request.word())
+                .originalLineText(request.originalText())
+                // .press(press)
+                .pressId(request.pressId())
+                .lineNumber(request.lineNumber())
+                .translatedWord(translateTextResponse.getTranslatedText())
+                .build();
 
         WordToLearn save = wordRepository.save(wordToLearn);
 
         return WordToLearnResponse.builder()
-            .id(save.getId())
-            .word(save.getWord())
-            .originalLineText(save.getOriginalLineText())
-            .lineNumber(save.getLineNumber())
-            .translatedWord(save.getTranslatedWord())
-            .build();
+                .id(save.getId())
+                .word(save.getWord())
+                .originalLineText(save.getOriginalLineText())
+                .lineNumber(save.getLineNumber())
+                .translatedWord(save.getTranslatedWord())
+                .build();
     }
 
     public List<WordToLearnResponse> getWordToLearn(Long pressId) {
         // user id, press id기반으로 유저가 모른다고 등록한 단어 가져오기
         Long userId = SecurityUtil.getUserId();
 
-        List<WordToLearn> wordsToLearn = wordRepository.findAllByUserIdAndPressId(userId,
-            pressId);
+        List<WordToLearn> wordsToLearn = wordRepository.findAllByUserIdAndPressIdOrderById(userId,
+                pressId);
 
         return WordToLearnResponse.listOf(wordsToLearn);
     }
@@ -81,5 +84,21 @@ public class WordService {
         List<WordToLearn> wordsToLearn = wordRepository.findAllByUserId(userId);
 
         return WordToLearnResponse.listOf(wordsToLearn);
+    }
+
+    @Transactional
+    public String updateWord(Long wordId, WordToLearnRequest translatedWord) {
+        Optional<WordToLearn> word = wordRepository.findById(wordId);
+        word.ifPresent(wordToLearn -> {
+            wordToLearn.updateTranslatedWord(translatedWord.word());
+        });
+
+        // 번경된 단어 리턴
+        return translatedWord.word();
+    }
+
+    public String deleteWord(Long wordId) {
+        wordRepository.deleteById(wordId);
+        return "success";
     }
 }
