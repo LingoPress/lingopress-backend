ce40189066ed58b2d9c45a71d3388cf4cab051fe
Kid-Chang <pch3310@gmail.com>
2024-03-31 13:40:49 +0900

    fix: 단어ID 순서로 정렬되도록 수정

--- src/main/java/com/kidchang/lingopress/word/WordRepository.java
+++ src/main/java/com/kidchang/lingopress/word/WordRepository.java
@@ -1,13 +1,14 @@
 package com.kidchang.lingopress.word;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WordRepository extends JpaRepository<WordToLearn, Long> {
 
-    List<WordToLearn> findAllByUserIdAndPressId(Long userId, Long pressId);
+    List<WordToLearn> findAllByUserIdAndPressIdOrderById(Long userId, Long pressId);
 
     List<WordToLearn> findAllByUserId(Long userId);
 }
