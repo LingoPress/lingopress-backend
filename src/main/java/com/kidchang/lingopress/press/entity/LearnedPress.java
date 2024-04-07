1747fb6f2958bd777a8e0b86b0bc4786e6bfb2d2
Kid-Chang <pch3310@gmail.com>
2024-04-07 16:51:45 +0900

    feat: LearnedPress 엔티티 decreaseTranslatedLineCount 메서드 추가

--- src/main/java/com/kidchang/lingopress/press/entity/LearnedPress.java
+++ src/main/java/com/kidchang/lingopress/press/entity/LearnedPress.java
@@ -2,13 +2,7 @@ package com.kidchang.lingopress.press.entity;
 
 import com.kidchang.lingopress._base.entity.BaseTimeEntity;
 import com.kidchang.lingopress.user.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -45,5 +39,9 @@ public class LearnedPress extends BaseTimeEntity {
 
     public void increaseTranslatedLineCount() {
         this.learnedContentLine++;
+    }
+
+    public void decreaseTranslatedLineCount() {
+        this.learnedContentLine--;
     }
 }
