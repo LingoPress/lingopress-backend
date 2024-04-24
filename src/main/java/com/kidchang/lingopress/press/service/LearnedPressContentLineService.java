690b8d621eeae0ffb6619ec74ba111e91d85d5e9
Kid-Chang <pch3310@gmail.com>
2024-04-24 15:39:57 +0900

    feat: 메모 기능 구현

--- src/main/java/com/kidchang/lingopress/press/service/LearnedPressContentLineService.java
+++ src/main/java/com/kidchang/lingopress/press/service/LearnedPressContentLineService.java
@@ -4,6 +4,7 @@ import com.kidchang.lingopress._base.constant.Code;
 import com.kidchang.lingopress._base.exception.BusinessException;
 import com.kidchang.lingopress._base.utils.SecurityUtil;
 import com.kidchang.lingopress.press.PressRepository;
+import com.kidchang.lingopress.press.dto.request.TranslateContentLineMemoRequest;
 import com.kidchang.lingopress.press.dto.request.TranslateContentLineRequest;
 import com.kidchang.lingopress.press.dto.response.PressContentLineResponse;
 import com.kidchang.lingopress.press.entity.LearnedPress;
@@ -73,6 +74,8 @@ public class LearnedPressContentLineService {
             if (learnedPressContentLine.getIsCorrect()) {
                 learnedPress.decreaseTranslatedLineCount();
             }
+
+            // 만약 checkPressContentLine메서드를 실행할 때 isCorrect를 안보내면 에러 발생
             learnedPressContentLine.setIsCorrect(request.isCorrect());
             learnedPressContentLine.setUserTranslatedLine(request.translateText());
         }
@@ -88,6 +91,62 @@ public class LearnedPressContentLineService {
                 .id(learnedPressContentLine.getId())
                 .isCorrect(learnedPressContentLine.getIsCorrect())
                 .userTranslatedLineText(learnedPressContentLine.getUserTranslatedLine())
+                .build();
+
+    }
+
+    @Transactional
+    public PressContentLineResponse writePressContentLineMemo(TranslateContentLineMemoRequest request) {
+        // TODO duplicate code 줄이기
+        Long pressId = request.pressId();
+        // 1. User 가져오기
+        Long userId = SecurityUtil.getUserId();
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(Code.NOT_FOUND_USER));
+
+        // 2. Press 가져오기
+        Press press = pressRepository.findById(pressId)
+                .orElseThrow(() -> new BusinessException(Code.PRESS_NOT_FOUND));
+
+
+        // 3. PressContentLine 가져오기
+        PressContentLine pressContentLine = pressContentLineRepository.findByPressIdAndLineNumber(
+                        pressId,
+                        request.contentLineNumber())
+                .orElseThrow(() -> new BusinessException(Code.PRESS_NOT_FOUND));
+
+        // 4. LearnedPress 가져오기
+        LearnedPress learnedPress = learnPressService.findOrCreateLearnedPress(user, press);
+
+        // 5. 기존에 LearnedPressContentLine 있는지 확인
+        LearnedPressContentLine learnedPressContentLine = learnedPressContentLineRepository
+                .findByLearnedPressAndPressContentLine(learnedPress, pressContentLine)
+                .orElse(null);
+        if (learnedPressContentLine == null) {
+            // isCorrect, userTranslatedLine을 null로 설정해서 가져온다.
+            learnedPressContentLine = LearnedPressContentLine.builder()
+                    .learnedPress(learnedPress)
+                    .pressContentLine(pressContentLine)
+                    .isCorrect(null)
+                    .lineNumber(request.contentLineNumber())
+                    .press(press)
+                    .user(user)
+                    .userTranslatedLine(null)
+                    // 메모 저장
+                    .memo(request.memo())
+                    .build();
+            learnedPressContentLineRepository.save(
+                    learnedPressContentLine);
+        } else {
+            // 메모 저장
+            learnedPressContentLine.setMemo(request.memo());
+        }
+
+
+        // 기록된 LearnedPressContentLine을 가져오고, 없으면 isCorrect를 null로 설정해서 가져온다.
+        return PressContentLineResponse.builder()
+                .id(learnedPressContentLine.getId())
+                .memo(learnedPressContentLine.getMemo())
                 .build();
 
     }
