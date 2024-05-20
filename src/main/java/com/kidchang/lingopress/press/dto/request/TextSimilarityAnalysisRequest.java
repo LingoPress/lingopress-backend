c83b95014ab9f23f72609e0fbd36d69a641ee260
Kid-Chang <pch3310@gmail.com>
2024-05-20 16:17:59 +0900

    feat: pressTranslationContentLine을 만들면서 필요하게된 originalText 인자 추가

--- src/main/java/com/kidchang/lingopress/press/dto/request/TextSimilarityAnalysisRequest.java
+++ src/main/java/com/kidchang/lingopress/press/dto/request/TextSimilarityAnalysisRequest.java
@@ -1,9 +1,15 @@
 package com.kidchang.lingopress.press.dto.request;
 
+import lombok.Builder;
+
+@Builder
 public record TextSimilarityAnalysisRequest(
         String original_text,
         String compared_text,
         Long press_id,
-        int line_number
+        int line_number,
+        // 원문
+        String originalText
+
 ) {
 }
