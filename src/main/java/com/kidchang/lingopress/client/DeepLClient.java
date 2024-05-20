900fdd8f3a8b1be5116be9d914e4f9d1ed0a162e
Kid-Chang <pch3310@gmail.com>
2024-05-20 15:35:01 +0900

    feat: 즉석 문장 번역을 위한 deepL 클라이언트

--- src/main/java/com/kidchang/lingopress/client/DeepLClient.java
+++ src/main/java/com/kidchang/lingopress/client/DeepLClient.java
@@ -1,14 +1,32 @@
 package com.kidchang.lingopress.client;
 
-import com.kidchang.lingopress.translate.dto.request.DeepLRequest;
-import com.kidchang.lingopress.translate.dto.response.DeepLResponse;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
+import com.deepl.api.DeepLException;
+import com.deepl.api.TextResult;
+import com.deepl.api.Translator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-@FeignClient(name = "deepl", url = "https://api-free.deepl.com")
-public interface DeepLClient {
+//@FeignClient(name = "deepl", url = "https://api-free.deepl.com")
+//public interface DeepLClient {
+//
+//    @PostMapping(value = "/v2/translate")
+//    DeepLResponse translate(DeepLRequest request);
+//
+//}
 
-    @PostMapping(value = "/v2/translate")
-    DeepLResponse translate(DeepLRequest request);
+@Component
+public class DeepLClient {
+
+    Translator translator;
+
+
+    public DeepLClient(@Value("${deepl.auth-key}") String apiKey) {
+        translator = new Translator(apiKey);
+    }
+
+    public TextResult translate(String text, String sourceLang, String targetLang) throws DeepLException, InterruptedException {
+        return translator.translateText(text, sourceLang, targetLang);
+    }
+
 
 }
