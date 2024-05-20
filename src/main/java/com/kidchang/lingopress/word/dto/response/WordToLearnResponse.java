1bdedc695086521568f9d72a11e5be0bc8fb82d4
Kid-Chang <pch3310@gmail.com>
2024-05-20 16:23:21 +0900

    feat: 기계번역된 문장이 없을때 해석해서 제공하기 위한 translatedLineText 리스폰스 속성 추가

--- src/main/java/com/kidchang/lingopress/word/dto/response/WordToLearnResponse.java
+++ src/main/java/com/kidchang/lingopress/word/dto/response/WordToLearnResponse.java
@@ -2,37 +2,39 @@ package com.kidchang.lingopress.word.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.kidchang.lingopress.word.WordToLearn;
-import java.util.List;
 import lombok.Builder;
+
+import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
 public record WordToLearnResponse(
-    Long id,
-    String word,
-    String translatedWord,
-    String originalLineText,
-    Integer lineNumber,
-    boolean isLearned,
-    Long pressId
+        Long id,
+        String word,
+        String translatedWord,
+        String originalLineText,
+        Integer lineNumber,
+        boolean isLearned,
+        Long pressId,
+        String translatedLineText
 ) {
 
 
     public static List<WordToLearnResponse> listOf(List<WordToLearn> wordsToLearn) {
         return wordsToLearn.stream()
-            .map(WordToLearnResponse::of)
-            .toList();
+                .map(WordToLearnResponse::of)
+                .toList();
     }
 
     private static WordToLearnResponse of(WordToLearn wordToLearn) {
         return WordToLearnResponse.builder()
-            .id(wordToLearn.getId())
-            .word(wordToLearn.getWord())
-            .translatedWord(wordToLearn.getTranslatedWord())
-            .originalLineText(wordToLearn.getOriginalLineText())
-            .lineNumber(wordToLearn.getLineNumber())
-            .isLearned(wordToLearn.isLearned())
-            .pressId(wordToLearn.getPressId())
-            .build();
+                .id(wordToLearn.getId())
+                .word(wordToLearn.getWord())
+                .translatedWord(wordToLearn.getTranslatedWord())
+                .originalLineText(wordToLearn.getOriginalLineText())
+                .lineNumber(wordToLearn.getLineNumber())
+                .isLearned(wordToLearn.isLearned())
+                .pressId(wordToLearn.getPressId())
+                .build();
     }
 }
