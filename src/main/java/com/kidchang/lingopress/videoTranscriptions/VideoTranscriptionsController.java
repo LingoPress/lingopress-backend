999f061afbfad5161448a961f0a28ce8a03ffa29
Kid-Chang <pch3310@gmail.com>
2024-07-05 22:56:08 +0900

    feat: POST /v1/video-transcriptions api 뼈대 제작

--- src/main/java/com/kidchang/lingopress/videoTranscriptions/VideoTranscriptionsController.java
+++ src/main/java/com/kidchang/lingopress/videoTranscriptions/VideoTranscriptionsController.java
@@ -1,6 +1,10 @@
 package com.kidchang.lingopress.videoTranscriptions;
 
+import com.kidchang.lingopress.videoTranscriptions.dto.VideoTranscriptionsRequest;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class VideoTranscriptionsController {
 
+    private final VideoTranscriptionsService videoTranscriptionsService;
+
+    @Operation(summary = "비디오 자막 생성 요청")
+    @PostMapping("")
+    public void requestVideoTranscription(
+            @RequestBody VideoTranscriptionsRequest request
+    ) {
+        // 비디오 자막 생성 요청
+        videoTranscriptionsService.requestVideoTranscription(request.language(), request.videoUrl());
+
+
+    }
+
 
 }
+
--- src/main/java/com/kidchang/lingopress/videoTranscriptions/VideoTranscriptionsService.java
+++ src/main/java/com/kidchang/lingopress/videoTranscriptions/VideoTranscriptionsService.java
@@ -4,5 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VideoTranscriptionsService {
-
+    public void requestVideoTranscription(String language, String videoUrl) {
+        // 비디오 자막 생성 요청
+    }
 }
