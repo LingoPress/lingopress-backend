53757ce35d4694b41c51a4456cec0cde74a71ff2
Kid-Chang <pch3310@gmail.com>
2024-07-07 16:41:02 +0900

    feat: responseVideoTranscription 메서드 press에 owner 정보 저장과 videoTranscriptions에 상태 업데이트 하도록 구현

--- src/main/java/com/kidchang/lingopress/videoTranscriptions/VideoTranscriptionsService.java
+++ src/main/java/com/kidchang/lingopress/videoTranscriptions/VideoTranscriptionsService.java
@@ -1,22 +1,35 @@
 package com.kidchang.lingopress.videoTranscriptions;
 
+import com.kidchang.lingopress._base.constant.Code;
+import com.kidchang.lingopress._base.exception.BusinessException;
+import com.kidchang.lingopress.press.PressRepository;
+import com.kidchang.lingopress.press.entity.Press;
+import com.kidchang.lingopress.user.User;
+import com.kidchang.lingopress.user.UserService;
 import com.kidchang.lingopress.videoTranscriptions.dto.VideoTranscriptionsResponse;
-import com.kidchang.lingopress.videoTranscriptions.messaging.MessageRequest;
 import com.kidchang.lingopress.videoTranscriptions.messaging.MessageSender;
+import com.kidchang.lingopress.videoTranscriptions.messaging.dto.MessageRequest;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class VideoTranscriptionsService {
     private final VideoTranscriptionsRepository videoTranscriptionsRepository;
     private final MessageSender messageSender;
+    private final PressRepository pressRepository;
+    private final UserService userService;
 
     public VideoTranscriptionsResponse requestVideoTranscription(String language, String videoUrl) {
         // 비디오 자막 생성 요청
+        User user = userService.getUser();
         VideoTranscriptions videoTranscriptions = videoTranscriptionsRepository.save(VideoTranscriptions.builder()
                 .language(language)
                 .videoUrl(videoUrl)
+                .user(user)
                 .build());
 
         // rabbitmq에 메시지 전송
@@ -30,5 +43,23 @@ public class VideoTranscriptionsService {
                 .id(videoTranscriptions.getId())
                 .processingStatus(VideoProcessingEnum.PROCESSING)
                 .build();
+    }
+
+    @Transactional
+    public void responseVideoTranscription(Long queueId, Long pressId) {
+        // 비디오 자막 생성 완료 응답 처리
+        Optional<VideoTranscriptions> videoTranscriptions = videoTranscriptionsRepository.findById(queueId);
+        User user = videoTranscriptions.get().getUser();
+
+        // pressId 로 press 조회 후 owner에 user 추가
+        // .addOwner(pressId, user);
+        Press press = pressRepository.findById(pressId)
+                .orElseThrow(() -> new BusinessException(Code.PRESS_NOT_FOUND));
+
+        System.out.println("@@" + press.getId() + " " + user.getId());
+        press.addOwner(user);
+        videoTranscriptions.get().updateProcessingStatus(VideoProcessingEnum.COMPLETED);
+
+        // 메일 보내기
     }
 }
