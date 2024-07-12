b660526c8292384611b2fa9a64ad74a44f885325
Kid-Chang <pch3310@gmail.com>
2024-07-12 10:32:19 +0900

    feat: rabbitmq를 통해 영상 처리를 하지 못했다는 메시지가 오면 그에 맞춰 메일을 보내고, VideoTranscriptions 테이블에 FAIL로 상태 변경

--- src/main/java/com/kidchang/lingopress/videoTranscriptions/messaging/MessageReceiver.java
+++ src/main/java/com/kidchang/lingopress/videoTranscriptions/messaging/MessageReceiver.java
@@ -6,38 +6,60 @@ import com.kidchang.lingopress._base.constant.Code;
 import com.kidchang.lingopress._base.exception.BusinessException;
 import com.kidchang.lingopress.videoTranscriptions.VideoTranscriptionsService;
 import com.kidchang.lingopress.videoTranscriptions.messaging.dto.MessageResponse;
+import com.rabbitmq.client.Channel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class MessageReceiver {
+public class MessageReceiver implements ChannelAwareMessageListener {
 
     private final VideoTranscriptionsService videoTranscriptionsService;
+    private final ObjectMapper objectMapper;
 
+    @Override
     @RabbitListener(queues = RabbitMQConfig.RESPONSE_QUEUE)
-    public void receiveResponse(String message) {
-        log.info("Received message: {}", message);
-
+    public void onMessage(Message message, Channel channel) throws Exception {
         try {
-            // ObjectMapper 인스턴스 생성
-            ObjectMapper objectMapper = new ObjectMapper();
+            String messageBody = new String(message.getBody());
+            log.info("Received message: {}", messageBody);
 
             // JSON 문자열을 MessageResponse 레코드로 변환
-            MessageResponse messageResponse = objectMapper.readValue(message, MessageResponse.class);
+            MessageResponse messageResponse = objectMapper.readValue(messageBody, MessageResponse.class);
 
             // 로그 출력
             log.info("ID: {}", messageResponse.queueId());
             log.info("Press ID: {}", messageResponse.pressId());
+            log.info("Is Success: {}", messageResponse.isSuccess());
 
-            if (!videoTranscriptionsService.responseVideoTranscription(messageResponse.queueId(), messageResponse.pressId())) {
-                log.error("메일 전송에 실패함: {}", message);
+            boolean mailSendStatus;
+            // 처리에 실패했다고 유저에게 말하고 videoTranscriptions 테이블에 status 수정
+            if (messageResponse.isSuccess()) {
+                mailSendStatus = videoTranscriptionsService.responseVideoTranscription(messageResponse.queueId(), messageResponse.pressId(), true);
+            } else {
+                log.error("비디오 작업에 실패함: {}", messageBody);
+                mailSendStatus = videoTranscriptionsService.responseVideoTranscription(messageResponse.queueId(), messageResponse.pressId(), false);
             }
 
+            if (!mailSendStatus) {
+                log.error("메일 전송에 실패함: {}", messageBody);
+            }
+
+            // 메시지 성공 처리 후 Ack
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+
         } catch (Exception e) {
+            log.error("메시지 처리 중 오류 발생: ", e);
+            String messageBody = new String(message.getBody());
+            log.error("에러가 발생한 메시지: {}", messageBody);
+
+            // 예외 발생 시 Nack 또는 Reject
+            channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
             throw new BusinessException(Code.RABBITMQ_MESSAGE_RECEIVE_ERROR, e);
         }
     }
--- src/main/java/com/kidchang/lingopress/videoTranscriptions/messaging/dto/MessageResponse.java
+++ src/main/java/com/kidchang/lingopress/videoTranscriptions/messaging/dto/MessageResponse.java
@@ -1,7 +1,11 @@
 package com.kidchang.lingopress.videoTranscriptions.messaging.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record MessageResponse(
         Long queueId,
-        Long pressId
+        Long pressId,
+        Boolean isSuccess
 ) {
 }
